a826f0b1388da3e996c1f0173e5b6ee485e875e9
Eshwar Nukala <eshwarnukala@microsoft.com>
2023-12-04 11:06:37 +0530

    day3 part 2

--- day3/main.go
+++ day3/main.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+type point struct {
+	x int
+	y int
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	defer file.Close()
@@ -20,6 +25,7 @@ func main() {
 		line := scanner.Text()
 		arr = append(arr, line)
 	}
+	gear := make(map[point][]int)
 	for i, value := range arr {
 		for j := 0; j < len(value); {
 			if !(value[j] >= '0' && value[j] <= '9') {
@@ -31,15 +37,14 @@ func main() {
 			for ; end < len(value) && value[end] >= '0' && value[end] <= '9'; end++ {
 				num = num*10 + int(value[end]-'0')
 			}
+			part := false
 			if (j-1 >= 0) && !(arr[i][j-1] >= '0' && arr[i][j-1] <= '9') && !(arr[i][j-1] == '.') {
 				ans += num
-				j = end
-				continue
+				part = true
 			}
 			if (end < len(value)) && !(arr[i][end] >= '0' && arr[i][end] <= '9') && !(arr[i][end] == '.') {
 				ans += num
-				j = end
-				continue
+				part = true
 			}
 			if i-1 >= 0 {
 				k := j - 1
@@ -49,12 +54,9 @@ func main() {
 					}
 					if (arr[i-1][k] != '.') && !(arr[i-1][k] >= '0' && arr[i-1][k] <= '9') {
 						ans += num
+						part = true
 						break
 					}
-				}
-				if k <= end {
-					j = end
-					continue
 				}
 
 			}
@@ -66,19 +68,56 @@ func main() {
 					}
 					if (arr[i+1][k] != '.') && !(arr[i+1][k] >= '0' && arr[i+1][k] <= '9') {
 						ans += num
+						part = true
 						break
 					}
 				}
-				if k <= end {
-					j = end
-					continue
-				}
 
+			}
+			if part {
+				if (j-1 >= 0) && arr[i][j-1] == '*' {
+					gear[point{x: i, y: j - 1}] = append(gear[point{x: i, y: j - 1}], num)
+				}
+				if (end < len(value)) && arr[i][end] == '*' {
+					gear[point{x: i, y: end}] = append(gear[point{x: i, y: end}], num)
+				}
+				if i-1 >= 0 {
+					k := j - 1
+					for ; k <= end; k++ {
+						if k < 0 || k >= len(value) {
+							continue
+						}
+						if arr[i-1][k] == '*' {
+							gear[point{x: i - 1, y: k}] = append(gear[point{x: i - 1, y: k}], num)
+						}
+					}
+
+				}
+				if i+1 < len(arr) {
+					k := j - 1
+					for ; k <= end; k++ {
+						if k < 0 || k >= len(value) {
+							continue
+						}
+						if arr[i+1][k] == '*' {
+							gear[point{x: i + 1, y: k}] = append(gear[point{x: i + 1, y: k}], num)
+						}
+					}
+
+				}
+				j = end
+				continue
 			}
 			j++
 
 		}
 	}
+	power := 0
+	for _, value := range gear {
+		if len(value) == 2 {
+			power += (value[0] * value[1])
+		}
+	}
 
-	fmt.Println(ans)
+	fmt.Println(power)
 }
